8d0f2fa9ab0e4f5f5670307e2fb6d8c4e386e4da
Timmer, Daan <daan.timmer+github@gmail.com>
2026-07-18 22:45:59 +0000

    refactor: update dependency management in CMake configuration

--- cpp/cmake/dependencies.cpm.cmake
+++ cpp/cmake/dependencies.cpm.cmake
@@ -1,52 +1,76 @@
-# ---------------------------------------------------------------------------
-# CPM – download on first configure if not already cached (standalone only)
-# ---------------------------------------------------------------------------
-if(CUCUMBER_QUERY_STANDALONE)
-    set(CPM_DOWNLOAD_VERSION 0.40.2)
-    set(CPM_DOWNLOAD_LOCATION
-        "${CMAKE_CURRENT_BINARY_DIR}/cmake/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
+# renovate: datasource=github-tags packageName=cpm-cmake/CPM.cmake versioning=semver
+set(CPM_DOWNLOAD_TAG_DIGEST v0.43.1 456cb6754daaa010d57444d0c8ce6d95ecf006ab)
+# renovate: datasource=github-tags packageName=cucumber/messages versioning=semver
+set(CUCUMBER_MESSAGES_TAG_DIGEST v34.0.2 5158f5280a38f949fa444f2009c35d6e120063a2)
+# renovate: datasource=github-tags packageName=google/googletest versioning=semver
+set(GOOGLE_TEST_TAG_DIGEST v1.17.0 52eb8108c5bdec04579160ae17225d66034bd723)
 
-    if(NOT EXISTS "${CPM_DOWNLOAD_LOCATION}")
-        message(STATUS "Downloading CPM.cmake ${CPM_DOWNLOAD_VERSION}…")
-        file(DOWNLOAD
-            "https://github.com/cpm-cmake/CPM.cmake/releases/download/v${CPM_DOWNLOAD_VERSION}/CPM.cmake"
-            "${CPM_DOWNLOAD_LOCATION}"
-            TLS_VERIFY ON
+list(GET CPM_DOWNLOAD_TAG_DIGEST 0 CPM_DOWNLOAD_TAG)
+list(GET CPM_DOWNLOAD_TAG_DIGEST 1 CPM_DOWNLOAD_DIGEST)
+string(SUBSTRING "${CPM_DOWNLOAD_TAG}" 1 -1 CPM_DOWNLOAD_VERSION)
+
+list(GET CUCUMBER_MESSAGES_TAG_DIGEST 0 CUCUMBER_MESSAGES_TAG)
+list(GET CUCUMBER_MESSAGES_TAG_DIGEST 1 CUCUMBER_MESSAGES_DIGEST)
+string(SUBSTRING "${CUCUMBER_MESSAGES_TAG}" 1 -1 CUCUMBER_MESSAGES_VERSION)
+
+list(GET GOOGLE_TEST_TAG_DIGEST 0 GOOGLE_TEST_TAG)
+list(GET GOOGLE_TEST_TAG_DIGEST 1 GOOGLE_TEST_DIGEST)
+string(SUBSTRING "${GOOGLE_TEST_TAG}" 1 -1 GOOGLE_TEST_VERSION)
+
+if(CUCUMBER_QUERY_FETCH_DEPS)
+    if(NOT COMMAND CPMAddPackage)
+        # ---------------------------------------------------------------------------
+        # CPM – download on first configure if not already cached (standalone only)
+        # ---------------------------------------------------------------------------
+        set(CPM_USE_LOCAL_PACKAGES ON)
+        set(CPM_DOWNLOAD_LOCATION "${CMAKE_CURRENT_BINARY_DIR}/cmake/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
+
+        if(NOT EXISTS "${CPM_DOWNLOAD_LOCATION}")
+            message(STATUS "Downloading CPM.cmake ${CPM_DOWNLOAD_VERSION}…")
+            file(DOWNLOAD
+                "https://github.com/cpm-cmake/CPM.cmake/releases/download/${CPM_DOWNLOAD_TAG}/CPM.cmake"
+                "${CPM_DOWNLOAD_LOCATION}"
+                TLS_VERIFY ON
+            )
+        endif()
+
+        include("${CPM_DOWNLOAD_LOCATION}")
+    endif()
+
+    # ---------------------------------------------------------------------------
+    # Dependencies
+    # ---------------------------------------------------------------------------
+    CPMAddPackage(
+        URI "gh:cucumber/messages@${CUCUMBER_MESSAGES_VERSION}#${CUCUMBER_MESSAGES_DIGEST}"
+        NAME cucumber_messages
+        SOURCE_SUBDIR cpp
+        OPTIONS
+            "CUCUMBER_MESSAGES_FETCH_DEPS ON"
+    )
+
+    if(cucumber_messages_ADDED)
+        # Propagate cucumber_messages install rules so 'cmake --install' also installs it
+        install(SCRIPT "${cucumber_messages_BINARY_DIR}/cmake_install.cmake")
+    endif()
+
+    if (CUCUMBER_QUERY_BUILD_TESTS)
+        CPMAddPackage(
+            URI "gh:google/googletest@${GOOGLE_TEST_VERSION}#${GOOGLE_TEST_DIGEST}"
+            NAME googletest
+            OPTIONS
+                "INSTALL_GTEST OFF"
+                "gtest_force_shared_crt ON"
+        )
+
+        set_target_properties(gtest gtest_main gmock gmock_main PROPERTIES
+            FOLDER External/GoogleTest
         )
     endif()
 
-    include("${CPM_DOWNLOAD_LOCATION}")
+else()
+    find_package(cucumber_messages ${CUCUMBER_MESSAGES_VERSION} REQUIRED)
+
+    if (CUCUMBER_QUERY_BUILD_TESTS)
+        find_package(GTest ${GOOGLE_TEST_VERSION} REQUIRED)
+    endif()
 endif()
-
-# ---------------------------------------------------------------------------
-# Dependencies
-# ---------------------------------------------------------------------------
-CPMAddPackage(
-    NAME nlohmann_json
-    GITHUB_REPOSITORY nlohmann/json
-    GIT_TAG v3.11.3
-    OPTIONS "JSON_BuildTests OFF" "JSON_Install ON"
-    SYSTEM
-)
-if(nlohmann_json_ADDED)
-    # Make the generated config file discoverable by cucumber-messages' find_package()
-    list(APPEND CMAKE_PREFIX_PATH "${nlohmann_json_BINARY_DIR}")
-endif()
-
-CPMAddPackage(
-    NAME cucumber-messages
-    GIT_REPOSITORY https://github.com/cucumber/messages.git
-    GIT_TAG        main
-    SOURCE_SUBDIR  cpp
-    SYSTEM
-)
-
-CPMAddPackage(
-    NAME googletest
-    GITHUB_REPOSITORY google/googletest
-    GIT_TAG v1.14.0
-    OPTIONS
-        "INSTALL_GTEST OFF"
-        "gtest_force_shared_crt ON"
-    SYSTEM
-)
